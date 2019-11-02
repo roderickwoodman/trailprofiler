dc33286f87a92544a916c0e8eaee23427819e6aa
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-11-02 11:47:46 -0700

    (WIP) Refactors original source into components, error-free

--- src/components/TrailDataGrid.vue
+++ src/components/TrailDataGrid.vue
@@ -57,11 +57,45 @@ export default {
   props: ['sequences'],
   data() {
     return {
+        units: "english"
     }
+  },
+  filters: {
+      to_datestring: function (epoch) {
+          if (!epoch) return ''
+          let converted = new Date(epoch);
+          return converted.toDateString();
+      },
+      to_hmm: function (seconds) {
+          let tot = Number(seconds);
+          let d = Math.floor(tot / 86400);
+          let h = Math.floor(tot % 86400 / 3600);
+          let m = Math.floor(tot % 86400 % 3600 / 60);
+          if (d < 1 && m < 10) {
+              m = '0' + m;
+          }
+          if (d > 0) {
+              return d + 'd ' + h + 'h ' + m + 'm';
+          } else {
+              return h + ':' + m;
+          }
+      },
+      to_tenths: function (number) {
+          return (Math.round(10*number)/10).toFixed(1);
+      },
+      to_units: function (meters) {
+          if (this.units === "english") {
+              return Math.round(meters * 3.28084);
+          } else {
+              return meters;
+          }
+      }
   }
 }
 
 </script>
+
+
 <style scoped>
     button {
         margin: 2px 6px;
