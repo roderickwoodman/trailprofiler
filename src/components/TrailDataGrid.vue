dd33d120412afcca62b44a4f0c53c45e16239129
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-11-05 14:23:07 -0800

    Extends the unit conversion method to also convert km to mi

--- src/components/TrailDataGrid.vue
+++ src/components/TrailDataGrid.vue
@@ -25,9 +25,9 @@
                         <button class="btn btn-xs btn-primary" v-on:click="clickedDeleteSequence(index)">d</button>
                     </td>
                     <td v-bind:class="{ wasSplit: sequence.was_split }">{{ sequence.total_time | to_hmm }}</td>
-                    <td v-bind:class="{ wasSplit: sequence.was_split }">{{ sequence.total_distance | to_tenths }}</td>
-                    <td v-bind:class="{ wasSplit: sequence.was_split }">{{ to_desired_units(sequence.minimum_elevation) }}</td>
-                    <td v-bind:class="{ wasSplit: sequence.was_split }">{{ to_desired_units(sequence.maximum_elevation) }}</td>
+                    <td v-bind:class="{ wasSplit: sequence.was_split }">{{ to_desired_units("km", sequence.total_distance) | to_tenths }}</td>
+                    <td v-bind:class="{ wasSplit: sequence.was_split }">{{ to_desired_units("m", sequence.minimum_elevation) }}</td>
+                    <td v-bind:class="{ wasSplit: sequence.was_split }">{{ to_desired_units("m", sequence.maximum_elevation) }}</td>
                 </tr>
             </tbody>
         </table>
@@ -45,11 +45,19 @@ export default {
     }
   },
   methods: {
-    to_desired_units: function (meters) {
-        if (this.units === "english") {
-            return Math.round(meters * 3.28084);
-        } else {
-            return meters;
+    to_desired_units: function (starting_units, value) {
+        if (starting_units === "m") {  // meters to feet
+            if (this.units === "english") {
+                return Math.round(value * 3.28084);
+            } else {
+                return value;
+            }
+        } else if (starting_units === "km") {  // kilometers to miles
+            if (this.units === "english") {
+                return Math.round(value * 0.621371);
+            } else {
+                return value;
+            }
         }
     }
   },
