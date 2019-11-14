c4ab3720375c7e9beca6ac1c86498ebe6a377781
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-11-13 18:57:52 -0800

    Formats some source files using Prettier

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # trailprofiler
+
 These days, so many of the devices that we have can record where we've gone. And seeing those tracks on a map is certainly nice. But what then? The next track will likely be on a different map. And so will the next one.
 
 But **Trailprofiler** lets you compare your journeys side-by-side in the only way that you can, with elevation profiles. It lets you manipulate your GPX data files with an easy Web interface. So you can quantify with quick visuals how much harder your last hike/run/bike really was.
@@ -6,24 +7,29 @@ But **Trailprofiler** lets you compare your journeys side-by-side in the only wa
 It also splits up data from files into separate series if it detects that they were actually recorded from different sessions.
 
 ## Built with Vue CLI...
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faChartLine } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Vue from 'vue';
+import App from './App.vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faChartLine} from '@fortawesome/free-solid-svg-icons';
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
+import {faInfoCircle} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
-library.add(faChartLine)
-library.add(faTrash)
-library.add(faInfoCircle)
+library.add(faChartLine);
+library.add(faTrash);
+library.add(faInfoCircle);
 
-Vue.use(require('vue-chartist'))
-Vue.use(BootstrapVue)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(require('vue-chartist'));
+Vue.use(BootstrapVue);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
