b2878fff5ad924a57f0beae14a3463ffb45d3cae
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-12-03 22:50:56 -0800

    Adds time to photo grid date column

--- src/components/TrailPhotosGrid.vue
+++ src/components/TrailPhotosGrid.vue
@@ -4,14 +4,14 @@
         <table class="table table-sm table-hover">
             <thead>
                 <tr>
-                    <th>Date</th>
-                    <th>File</th>
+                    <th>Date - Time</th>
+                    <th>Filename</th>
                     <th>Camera</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-bind:key="photo.uuid" v-for="photo in photos">
-                    <td>{{ photo.exif.DateTime | to_datestring_from_ymdhms }}</td>
+                    <td>{{ photo.exif.DateTime | to_datetimestring_from_ymdhms }}</td>
                     <td>{{ photo.filename }}</td>
                     <td>{{ photo.exif.Make }} {{ photo.exif.Model }}</td>
                 </tr>
@@ -26,12 +26,16 @@
 export default {
 	props: ['photos'],
 	filters: {
-		to_datestring_from_ymdhms: function (ymdhms) { // expect "yyyy:mm:dd hh:mm:ss"
+		to_datetimestring_from_ymdhms: function (ymdhms) { // expect "yyyy:mm:dd hh:mm:ss"
 			if (!ymdhms) return '';
 			let params = ymdhms.replace(' ',':').split(':');
 			params[1] = parseInt(params[1]) - 1;
-			let converted = new Date(...params);
-			return converted.toDateString();
+			let date = new Date(...params);
+			const leadingZero = (num) => (0 + num.toString()).slice(-2);
+			const formatTime = function(date) {
+				return [date.getHours(), date.getMinutes(), date.getSeconds()].map(leadingZero).join(':');
+			};
+			return date.toDateString() + ' - ' + formatTime(date);
 		}
 	}
 };
