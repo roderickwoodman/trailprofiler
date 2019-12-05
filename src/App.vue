f355b1eb0219f47dc075d3dba7cf5bd11f41339b
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-12-04 20:57:17 -0800

    Adds end time attribute for each sequence

--- src/App.vue
+++ src/App.vue
@@ -321,6 +321,7 @@ export default {
 						new_segment['total_time'] = segment_time_aggrs[segment_time_aggrs.length - 1];
 						new_segment['average_pace'] = new_segment.total_time / new_segment.total_distance; // seconds per km
 						new_segment['start_time'] = new_segment.points[0].time;
+						new_segment['end_time'] = new_segment.points[new_segment.points.length - 1].time;
 						new_segment['maximum_elevation'] = segment_max_ele;
 						new_segment['minimum_elevation'] = segment_min_ele;
 						// eslint-disable-next-line no-console
@@ -343,6 +344,7 @@ export default {
 						wholefile['total_time'] = wholefile_time_aggrs[wholefile_time_aggrs.length - 1];
 						wholefile['average_pace'] = wholefile.total_time / wholefile.total_distance; // seconds per km
 						wholefile['start_time'] = wholefile.points[0].time;
+						wholefile['end_time'] = wholefile.points[new_segment.points.length - 1].time;
 						wholefile['maximum_elevation'] = wholefile_max_ele;
 						wholefile['minimum_elevation'] = wholefile_min_ele;
 						// eslint-disable-next-line no-console
