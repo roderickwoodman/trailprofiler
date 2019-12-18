29c4debbeffb0583f8826295940e856ba61e5f9e
Rod Woodman <roderickwoodman@users.noreply.github.com>
2019-12-17 21:19:52 -0800

    Adds camera specs as an overlay on each photo

--- src/components/RowOfPhotos.vue
+++ src/components/RowOfPhotos.vue
@@ -1,8 +1,15 @@
 <template>
 	<div class="row_of_photos">
 		<div v-if="!row_photos.length" class="info_message"><font-awesome-icon icon="info-circle" /> this sequence has no photos </div>
-		<div v-bind:key="photo.uuid" v-for="photo in sorted_photos" class="photo_container">
-			<img :src="photo.filename">
+		<div v-bind:key="photo.uuid" v-for="photo in sorted_photos" class="photo_item">
+			<div class="photo_container">
+				<img :src="photo.filename">
+				<figcaption class="photo_overlay">
+					<div>{{ photo.aperture_printable }}</div>
+					<div>{{ photo.shutter_printable }}</div>
+					<div>{{ photo.iso }}</div>
+				</figcaption>
+			</div>
 			<figcaption v-if="time_format === 'epoch'">{{ photo.datetime }}</figcaption>
 			<figcaption v-if="time_format !== 'epoch' && show_date">{{epoch_to_datestring(photo.datetime)}}</figcaption>
 			<figcaption v-if="time_format !== 'epoch'">{{epoch_to_timestring(photo.datetime)}}</figcaption>
@@ -44,7 +51,7 @@ export default {
 		width: 100%;
 		text-align: center;
 	}
-	.photo_container {
+	.photo_item {
 		display: flex;
 		justify-content: flex-start;
 		flex-direction: column;
@@ -54,6 +61,27 @@ export default {
 		width: 100px;
 		height: 100%;
 		text-align: center;
+	}
+	.photo_container {
+		width: 75px;
+		height: 75px;
+		position: relative;
+		background-color: black;
+	}
+	.photo_container > img {
+		opacity: 0.7;
+		width: 100%;
+		height: 100%;
+	}
+	.photo_overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		padding: 5px;
+		color: white;
+		font-size: 0.7em;
+		font-weight: 800;
+		text-align: left;
 	}
 	figcaption {
 		font-size: 0.8em;
